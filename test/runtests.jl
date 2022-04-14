b21a1be552983f74b34bd3006f887324651707f5
Beat Hubmann <BeatHubmann@users.noreply.github.com>
2022-04-14 20:29:59 +0200

    Complete node bilinear interpolation tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using HydrologyPlanetesimals
 using Test
 using StaticArrays
 
-@testset verbose = false "HydrologyPlanetesimals.jl" begin
+@testset verbose = true "HydrologyPlanetesimals.jl" begin
 
-    @testset "distance(x1, y1, x2, y2)" begin
+    @testset "distance(x1,y1,x2,y2)" begin
         @test HydrologyPlanetesimals.distance(0, 0, 0, 0) == 0
         @test HydrologyPlanetesimals.distance(1, 0, 0, 0) == 1
         @test HydrologyPlanetesimals.distance(0, 1, 0, 0) == 1
@@ -15,7 +15,7 @@ using StaticArrays
         @test HydrologyPlanetesimals.distance(-1, -1, 1, 1) ≈ sqrt(8)
     end
 
-    @testset "total(solid, fluid, phi)" begin
+    @testset "total(solid,fluid,phi)" begin
         @test HydrologyPlanetesimals.total(0, 0, 0) == 0
         @test HydrologyPlanetesimals.total(1, 0, 0) == 1
         @test HydrologyPlanetesimals.total(0, 1, 0) == 0
@@ -23,19 +23,19 @@ using StaticArrays
         @test HydrologyPlanetesimals.total(1, 2, 0.5) == 1.5
     end
 
-    @testset "ktotal(ksolid, kfluid, phi)" begin
+    @testset "ktotal(ksolid,kfluid,phi)" begin
         # from madcph.m, line 1761
         ktotalm(ksolidm, kfluid, phim)=(ksolidm*kfluid/2+((ksolidm*(3*phim-2)+kfluid*(1-3*phim))^2)/16)^0.5-(ksolidm*(3*phim-2)+ kfluid*(1-3*phim))/4
         @test HydrologyPlanetesimals.ktotal(1., 2., 3.) == ktotalm(1., 2., 3.)
     end
 
-    @testset "kphi(kphim0, phim, phim0)" begin
+    @testset "kphi(kphim0,phim,phim0)" begin
         # from madcph.m, line 333
         kphim(kphim0, phim, phim0)=kphim0*(phim/phim0)^3/((1-phim)/(1-phim0))^2
         @test HydrologyPlanetesimals.kphi(1., 2., 3.) == kphim(1., 2., 3.)
     end
 
-    @testset "Q_radiogenic(f, ratio, E, tau, time)" begin
+    @testset "Q_radiogenic(f,ratio,E,tau,time)" begin
         # from madcph.m, line 276
         Q(f, ratio, E, tau, timesum)=f*ratio*E*exp(-timesum/tau)/tau
         @test HydrologyPlanetesimals.Q_radiogenic(1., 2., 3., 4., 5.) == Q(
@@ -44,7 +44,7 @@ using StaticArrays
             1., 2., 3., 4., 0.)
     end
 
-    @testset "calculate_radioactive_heating(timesum, sp)" begin
+    @testset "calculate_radioactive_heating(timesum,sp)" begin
         hr_al = false
         hr_fe = false
         v = @SVector [0., 0., 0.]
