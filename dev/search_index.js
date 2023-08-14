b2b714baeca2dd7195158b4bc4e1e4a8fde6935c
Documenter.jl <documenter@juliadocs.github.io>
2023-08-14 13:30:49 +0000

    build based on 7eb2a05

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Erebus.jl","page":"Home","title":"Erebus.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Erebus.jl","category":"page"}]
+}
