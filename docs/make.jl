7eb2a05b99b8d1e3734e95d81930d9a8af2dd292
Beat Hubmann <BeatHubmann@users.noreply.github.com>
2023-08-14 15:26:20 +0200

    Add docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,21 @@ using Documenter
 using Erebus
 
 makedocs(
-    sitename = "Erebus",
-    format = Documenter.HTML(),
-    modules = [Erebus]
+    sitename = "Erebus.jl",
+    modules = [Erebus],
+    authors="Beat Hubmann",
+    repo="https://github.com/FormingWorlds/Erebus.jl/blob/{commit}{path}#{line}",
+    format = Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/FormingWorlds/Erebus.jl",
+    devbranch="main",
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
