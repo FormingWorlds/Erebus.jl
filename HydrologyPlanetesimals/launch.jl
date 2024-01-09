cb0a0701f138ce05a629528ca4fcca79236f5cb4
Beat Hubmann <BeatHubmann@users.noreply.github.com>
2024-01-09 16:58:44 +0000

    Remove MKL dependency

--- HydrologyPlanetesimals/launch.jl
+++
@@ -1,3 +0,0 @@
-using HydrologyPlanetesimals
-
-run_simulation()
--- HydrologyPlanetesimals/reference/load_jld2.m
+++
@@ -1,8 +0,0 @@
-function [data_2] = load_jld2(data, step)
-%load_mat Load jld2 version of data .jld2 at given step
-%     path_2 = 'C:\Users\ich\outTest\';
-    path_2 = '/Users/z7717/test/';
-    name_2 = 'M_1078_';
-    root_file_2 = [path_2 name_2 num2str(step) '.jld2'];
-    data_2 = h5read(root_file_2, ['/' data]);
-end
